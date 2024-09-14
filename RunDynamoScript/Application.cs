073a1893996f078a6c20074fb0035fb31a427909
skibadubskiybadubs <brovarch@gmail.com>
2024-09-14 16:59:37 +0300

    Run Dynamo Script from Button 21
    This installs a plugin for Revit 21.1.2, creating a ribbon button that allows to run a Dynamo script.

---
+++ RunDynamoScript/Application.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Reflection;
+using System.Windows.Media.Imaging;
+using System.IO;
+using System.Windows;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+
+namespace RunDynamoScript
+{
+    public class Application : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication application)
+        {
+            string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
+            string pluginLoc = thisAssemblyPath;
+
+            String tabName = "RunDynamoScript";
+            application.CreateRibbonTab(tabName);
+            RibbonPanel ribbonPanel = application.CreateRibbonPanel(tabName, "run");
+            
+            PushButtonData buttonData = new PushButtonData("Apply", "ApplyParameters", pluginLoc, "RunDynamoScript.RunDynamo");
+            PushButton pushButton = ribbonPanel.AddItem(buttonData) as PushButton;
+            Uri uri = new Uri(Path.Combine(Path.GetDirectoryName(pluginLoc), "apply.png"));
+            BitmapImage bitmapImage = new BitmapImage(uri);
+            pushButton.LargeImage = bitmapImage;
+            pushButton.ToolTip = "Apply Properties";
+
+
+            //RibbonController.ShowOptionsBar(null);
+            //ShowUtilsBarCommand showUtilsBarCommand = new ShowUtilsBarCommand();
+
+            // PushButtonData okButtonData = new PushButtonData("cmdOk", "終了", pluginLoc, "BimFanParking.OkCommand"); // Ok
+            // PushButtonData cancelButtonData = new PushButtonData("cmdCancel", "キャンセル", pluginLoc, "BimFanParking.CancelCommand"); // Cancel
+
+            // IList<RibbonItem> stackedItems = ribbonPanel.AddStackedItems(okButtonData, cancelButtonData);
+
+            // // Store the PushButton objects
+            // OkButton = stackedItems[0] as PushButton;
+            // CancelButton = stackedItems[1] as PushButton;
+
+            // // Disable the buttons
+            // OkButton.Visible = false;
+            // CancelButton.Visible = false;
+
+            return Result.Succeeded;
+        }
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+        public void EnableButtons()
+        {
+            //OkButton.Visible = true;
+            //CancelButton.Visible = true;
+        }
+    }
+}
---
+++ RunDynamoScript/RunDynamo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Dynamo.Applications;
+using Dynamo.Core;
+using RevitServices;
+using static System.Net.Mime.MediaTypeNames;
+using Document = Autodesk.Revit.DB.Document;
+
+namespace RunDynamoScript
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class RunDynamo : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
+            string pluginLoc = thisAssemblyPath;
+
+            //Get application and documnet objects and start transaction
+            UIApplication uiapp = commandData.Application;
+            Document doc = uiapp.ActiveUIDocument.Document;
+
+            //string Journal_Dynamo_Path = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources\\script.dyn");
+            string Journal_Dynamo_Path = Path.Combine(Path.GetDirectoryName(pluginLoc), "script.dyn");
+            DynamoRevit dynamoRevit = new DynamoRevit();
+
+            DynamoRevitCommandData dynamoRevitCommandData = new DynamoRevitCommandData();
+            dynamoRevitCommandData.Application = commandData.Application;
+            IDictionary<string, string> journalData = new Dictionary<string, string>
+            {
+                { Dynamo.Applications.JournalKeys.ShowUiKey, false.ToString() }, // don't show DynamoUI at runtime
+                { Dynamo.Applications.JournalKeys.AutomationModeKey, true.ToString() }, //run journal automatically
+                { Dynamo.Applications.JournalKeys.DynPathKey, Journal_Dynamo_Path }, //run node at this file path
+                { Dynamo.Applications.JournalKeys.DynPathExecuteKey, true.ToString() }, // The journal file can specify if the Dynamo workspace opened from DynPathKey will be executed or not. If we are in automation mode the workspace will be executed regardless of this key.
+                { Dynamo.Applications.JournalKeys.ForceManualRunKey, false.ToString() }, // don't run in manual mode
+                { Dynamo.Applications.JournalKeys.ModelShutDownKey, true.ToString() },
+                { Dynamo.Applications.JournalKeys.ModelNodesInfo, false.ToString() }
+
+            };
+
+            dynamoRevitCommandData.JournalData = journalData;
+            Result externalCommandResult = dynamoRevit.ExecuteCommand(dynamoRevitCommandData);
+            return externalCommandResult;
+
+        }
+    }
+}
